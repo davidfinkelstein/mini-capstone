8ed0093830438dc0f382a32fd0cac638fcc45f00
David <davidjosephfinkelstein@gmail.com>
2018-08-29 14:58:38 -0400

    controller and view added to products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,7 @@
-class Api::ProductsController < ApplicationController
+class Api::ProductsController < ApplicationController #Api:: <=== namespace
+
+  def product_method
+    @products = Product.all
+    render 'shop.json.jbuilder'
+  end
 end
